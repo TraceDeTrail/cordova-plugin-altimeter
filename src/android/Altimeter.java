da0986b4f2accafa4c5cea1ccdb8944497f64820
TraceDeTrail <admin@tracedetrail.fr>
2017-10-01 23:09:46 +0200

    test12

--- src/android/Altimeter.java
+++ src/android/Altimeter.java
@@ -2,7 +2,7 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
-package com.tracedetrail.cordova.altimeter;
+package com.tracedetrail.cordova.Altimeter;
 
 import java.util.List;
 
