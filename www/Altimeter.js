fdbc0372ea3bf2eff607ca9743af098c2ab887b2
TraceDeTrail <admin@tracedetrail.fr>
2017-10-01 21:37:14 +0200

    test7

--- www/Altimeter.js
+++ www/Altimeter.js
@@ -1,21 +1,20 @@
 /********* Altimeter.js Cordova Plugin Implementation *******/
 
-var exec = require("cordova/exec");
 
 var Altimeter = function () {
     this.name = "Altimeter";
 };
 
 Altimeter.prototype.isAltimeterAvailable = function (onSuccess, onError) {
-    exec(function(rep){alert(rep)}, onError, "Altimeter", "isAltimeterAvailable", []);
+    cordova.exec(function(rep){alert('test');}, onError, "Altimeter", "isAltimeterAvailable", []);
 };
 
 Altimeter.prototype.startAltimeterUpdates = function (onSuccess, onError) {
-    exec(onSuccess, onError, "Altimeter", "startAltimeterUpdates", []);
+    cordova.exec(onSuccess, onError, "Altimeter", "startAltimeterUpdates", []);
 };
 
 Altimeter.prototype.stopAltimeterUpdates = function (onSuccess, onError) {
-    exec(onSuccess, onError, "Altimeter", "stopAltimeterUpdates", []);
+    cordova.exec(onSuccess, onError, "Altimeter", "stopAltimeterUpdates", []);
 };
 
 module.exports = new Altimeter();
