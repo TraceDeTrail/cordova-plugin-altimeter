1af280ee0b0357fd6a5ab6f06b40b1d376e9b087
TraceDeTrail <admin@tracedetrail.fr>
2017-10-02 07:29:17 +0200

    test15

--- src/android/Altimeter.java
+++ src/android/Altimeter.java
@@ -28,7 +28,39 @@ import android.os.Looper;
  * This class listens to the barometer sensor and stores the latest
  * pressure value.
  */
-public class Altimeter extends CordovaPlugin {
+public class Altimeter extends CordovaPlugin implements SensorEventListener {
+
+    private float pressure;  // most recent pressure value
+    private float relativeAltitude;  // most recent pressure value
+    private float firstAltitude;
+    private int accuracy = SensorManager.SENSOR_STATUS_UNRELIABLE;
+
+    private SensorManager sensorManager;  // Sensor manager
+    private Sensor mSensor;  // Pressure sensor returned by sensor manager
+
+    private CallbackContext callbackContext;  // Keeps track of the JS callback context.
+
+    /**
+     * Create an barometer listener.
+     */
+    public Altimeter() {
+        this.pressure = 0;
+        this.relativeAltitude = 0;
+        this.firstAltitude = 100000;
+     }
+
+    /**
+     * Sets the context of the Command. This can then be used to do things like
+     * get file paths associated with the Activity.
+     *
+     * @param cordova The context of the main Activity.
+     * @param webView The associated CordovaWebView.
+     */
+    @Override
+    public void initialize(CordovaInterface cordova, CordovaWebView webView) {
+        super.initialize(cordova, webView);
+        this.sensorManager = (SensorManager) cordova.getActivity().getSystemService(Context.SENSOR_SERVICE);
+    }
 
     /**
      * Executes the request.
@@ -39,15 +71,147 @@ public class Altimeter extends CordovaPlugin {
      * @return              Whether the action was valid.
      */
     public boolean execute(String action, JSONArray args, CallbackContext callbackContext) {
-            String responseText = "Hello world";
-            callbackContext.success(responseText);
-          //  PluginResult result = new PluginResult(PluginResult.Status.OK, 'test');
-          //  result.setKeepCallback(true);
-            //callbackContext.sendPluginResult(result);
+        this.callbackContext = callbackContext;
+        if(action.equals("isAltimeterAvailable")){
+          List<Sensor> list = this.sensorManager.getSensorList(Sensor.TYPE_PRESSURE);
+          if ((list != null) && (list.size() > 0)) {
+            PluginResult result = new PluginResult(PluginResult.Status.OK, true);
+            result.setKeepCallback(true);
+            callbackContext.sendPluginResult(result);
+          }
+          else {
+            PluginResult result = new PluginResult(PluginResult.Status.OK, false);
+            result.setKeepCallback(true);
+            callbackContext.sendPluginResult(result);
+          }
+        }
+        else if(action.equals("stopAltimeterUpdates")){
+            this.stop();
+            PluginResult result = new PluginResult(PluginResult.Status.OK, "");
+            result.setKeepCallback(true);
+            callbackContext.sendPluginResult(result);
+        }
+        else if(action.equals("startAltimeterUpdates")){
+            this.start();
+        }
         return true;
     }
 
+    /**
+     * Called by BarometerBroker when listener is to be shut down.
+     * Stop listener.
+     */
+    public void onDestroy() {
+        this.stop();
+    }
+
+    //--------------------------------------------------------------------------
+    // LOCAL METHODS
+    //--------------------------------------------------------------------------
+    //
+    /**
+     * Start listening for pressure sensor.
+     *
+    */
+    private void start() {
+
+        List<Sensor> list = this.sensorManager.getSensorList(Sensor.TYPE_PRESSURE);
+        this.mSensor = list.get(0);
+        this.sensorManager.registerListener(this, this.mSensor, SensorManager.SENSOR_DELAY_UI);
+
+    }
+
+    /**
+     * Stop listening to barometer sensor.
+     */
+    private void stop() {
+        this.sensorManager.unregisterListener(this);
+        this.accuracy = SensorManager.SENSOR_STATUS_UNRELIABLE;
+        this.pressure = 0;
+        this.relativeAltitude = 0;
+        this.firstAltitude = 100000;
+    }
+
+    /**
+     * Called when the accuracy of the sensor has changed.
+     *
+     * @param sensor
+     * @param accuracy
+     */
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+        // Only look at barometer events
+        if (sensor.getType() != Sensor.TYPE_PRESSURE) {
+            return;
+        }
+
+        this.accuracy = accuracy;
+    }
+
+    /**
+     * Sensor listener event.
+     *
+     * @param SensorEvent event
+     */
+    public void onSensorChanged(SensorEvent event) {
+        // Only look at barometer events
+        if (event.sensor.getType() != Sensor.TYPE_PRESSURE) {
+            return;
+        }
+
+        if (this.accuracy >= SensorManager.SENSOR_STATUS_ACCURACY_MEDIUM) {
+
+            // Save time that event was received
+            this.pressure = event.values[0];
+            if(this.firstAltitude == 100000){
+              this.firstAltitude=this.sensorManager.getAltitude(SensorManager.PRESSURE_STANDARD_ATMOSPHERE, this.pressure);
+              this.relativeAltitude=0;
+            }
+            else{
+              this.relativeAltitude = this.sensorManager.getAltitude(SensorManager.PRESSURE_STANDARD_ATMOSPHERE, this.pressure) - this.firstAltitude;
+            }
+            this.win();
+        }
+    }
+
+    /**
+     * Called when the view navigates.
+     */
+    @Override
+    public void onReset() {
+        this.stop();
+    }
+
+    // Sends an error back to JS
+    private void fail(int code, String message) {
+        // Error object
+        JSONObject errorObj = new JSONObject();
+        try {
+            errorObj.put("code", code);
+            errorObj.put("message", message);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        PluginResult err = new PluginResult(PluginResult.Status.ERROR, errorObj);
+        err.setKeepCallback(true);
+        this.callbackContext.sendPluginResult(err);
+    }
+
+    private void win() {
+        // Success return object
+        PluginResult result = new PluginResult(PluginResult.Status.OK, this.getPressureJSON());
+        result.setKeepCallback(true);
+        this.callbackContext.sendPluginResult(result);
+    }
 
 
-
+    private JSONObject getPressureJSON() {
+        JSONObject r = new JSONObject();
+        try {
+            r.put("pressure", this.pressure);
+            r.put("relativeAltitude", this.relativeAltitude);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        return r;
+    }
 }
